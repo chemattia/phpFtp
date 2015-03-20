23fe88833589d603733b4054d10cf12da067f980
Mattia Fontana <donkeyMat@MacBook-Air-di-Mattia.local>
2015-03-20 13:21:56 +0100

    Conversion to OPP paradigm

--- connection/ftp.php
+++ connection/ftp.php
@@ -1,28 +1,35 @@
 <?php
 
-$host = '';
-$user = '';
-$password = '';
-
-function downloadFtp($name_file)
+class Ftp
 {
-    global $host, $user, $password;
-    $connection = ftp_connect($host);
-    $login = ftp_login($connection, $user, $password);
-    if (!$connection || !$login) {
-        return false;
+    private $host;
+    private $user;
+    private $password;
+
+    public function setConnection($host, $user, $password)
+    {
+      $this->host = $host;
+      $this->user = $user;
+      $this->password = $password;
     }
-    else {
-        $local_file = 'files/' . $name_file . '.txt';
-        $server_file = $name_file . '.txt';
-        if (ftp_get($connection, $local_file, $server_file, FTP_BINARY))
-        {
-            return true;
-        }
-        else
-        {
+
+    public function downloadFtp($name_file)
+    {
+        $connection = ftp_connect($this->host);
+        $login = ftp_login($connection, $this->user, $this->password);
+        if (!$connection || !$login) {
             return false;
         }
-        ftp_close($connection);
+        else {
+            $local_file = 'files/' . $name_file . '.txt';
+            $server_file = $name_file . '.txt';
+            if (ftp_get($connection, $local_file, $server_file, FTP_BINARY)) {
+                return true;
+            } else {
+                return false;
+            }
+            ftp_close($connection);
+        }
     }
+
 }
--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-include 'connection/ftp.php';
+require 'connection/ftp.php';
 parse_str($_SERVER['QUERY_STRING']);
 if (isset($id_gruppo)) {
     $id_gruppo = (htmlspecialchars($id_gruppo, ENT_QUOTES));
-    if (downloadFtp($id_gruppo) == true) {
+    $ftp = new Ftp();
+    $ftp->setConnection('', '', '');
+    if ($ftp->downloadFtp($id_gruppo) == true) {
         echo 'id gruppo = ' . $id_gruppo . '<br>';
         ?>
         <form>
