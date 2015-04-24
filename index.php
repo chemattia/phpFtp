48eb003e407e1d163c6cc1d088a2e2f0863f8b98
Francesco Abeni <f.abeni@gibilogic.com>
2015-04-24 17:27:11 +0200

    Minor HTML modifications

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
     <head>
         <title>Mailupper</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-        <!-- link rel="stylesheet" href="css/mailupper.css" -->
-
-        <!-- script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script -->
+        <link rel="stylesheet" href="css/mailupper.css">
         <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
         <script src="js/mailupper.js"></script>
     </head>
@@ -17,12 +15,12 @@
             </section>
         </header>
         <hr />
-        <section>
+        <section id="main">
             <div id="welcome" class="message"><strong>Benvenuto!</strong><br>Verifico la richiesta... <span class="result"></span></div>
             <div id="download" class="message hidden">Scarico il file... <span class="result"></span></div>
             <div id="process" class="message hidden">Elaboro il file... <span class="result"></span></div>
             <div id="import" class="message hidden">Carico i destinatari... <span class="result"></span></div>
-            <div id="finish" class="message hidden">Completato! E' stato creato il nuovo gruppo: <span class="result"></span></div>
+            <div id="finish" class="message hidden">Completato! E' stato creato il nuovo gruppo: Automatic <span class="result"></span></div>
         </section>
         <hr />
         <footer>
